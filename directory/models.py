62c13a24a9518c78a35cb6b6dbb772a42afef97b
htira-abderrazak <abderrazak.htira@gmail.com>
2024-01-11 20:52:17 +0100

    add created and update dates in file and folders

--- directory/models.py
+++ directory/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 import uuid
+from datetime import date
 
 # Create your models here.
 class Directory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20,blank =True)
     parent = models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
--- file/models.py
+++ file/models.py
@@ -5,17 +5,14 @@ import os
 from datetime import date
 
 def get_upload_path(instance, filename):
-    # Get today's date in the format 'YYYY-MM-DD'
     today_date = date.today().strftime('%Y-%m-%d')
     
-    # Construct the upload path using the date
     upload_path = os.path.join('uploads', today_date)
 
     # Check if the folder exists, create it if not
     if not os.path.exists(upload_path):
         os.makedirs(upload_path)
 
-    # Return the final file path
     return os.path.join(upload_path, filename)
 
 
@@ -24,3 +21,5 @@ class File(models.Model):
     name = models.CharField(max_length=20,blank =True)
     directory = models.ForeignKey(Directory,on_delete=models.CASCADE)
     file = models.FileField(upload_to=get_upload_path, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
