32f6984bf75f49d940836bbb6e24b706707213b1
htira-abderrazak <abderrazak.htira@gmail.com>
2024-01-12 13:40:42 +0100

    fix : prevent same name in the same directory

--- directory/serializers.py
+++ directory/serializers.py
@@ -5,4 +5,14 @@ class DirectorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Directory
-        fields = '__all__'
+        fields = ['id','name','parent','created_at','updated_at']
+    def create(self, validated_data):
+        if validated_data["parent"]:
+            directories = Directory.objects.filter(parent = validated_data["parent"],name = validated_data["name"])
+        else:
+            directories = Directory.objects.filter(parent = None,name = validated_data["name"])
+        if(directories.exists()):
+            raise serializers.ValidationError(
+                {"name": "this name already exists."}
+            )
+        return super().create(validated_data)
--- file/serializers.py
+++ file/serializers.py
@@ -6,4 +6,16 @@ class FileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = File
-        fields =['name','directory','file']
+        fields =['id','name','directory','file']
+
+    file = serializers.FileField(required = True)
+    def create(self, validated_data):
+        if validated_data["directory"]:
+            directories = File.objects.filter(directory = validated_data["directory"],name = validated_data["name"])
+        else:
+            directories = File.objects.filter(directory = None,name = validated_data["name"])
+        if(directories.exists()):
+            raise serializers.ValidationError(
+                {"name": "this name already exists."}
+            )
+        return super().create(validated_data)
