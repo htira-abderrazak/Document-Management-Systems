23baec7f6c556df9f5be3b666bee3d80dca10877
htira-abderrazak <abderrazak.htira@gmail.com>
2024-01-20 19:26:35 +0100

    feat : file must not exceeds 10MB

--- file/serializers.py
+++ file/serializers.py
@@ -24,3 +24,12 @@ class FileSerializer(serializers.ModelSerializer):
         if os.path.isfile(instance.file.path):
             os.remove(instance.file.path)
         return super().update(instance, validated_data)
+    file = serializers.FileField()
+
+    def validate_file(self, value):
+        # Access the file size
+        file_size = value.size
+        max_file_size = 10 * 1024 * 1024  # 10 MB
+        if file_size > max_file_size:
+            raise serializers.ValidationError("File size exceeds the allowed limit.")
+        return value
