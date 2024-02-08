52b724cc6b3fa521f552a750defab9fef746c863
htira-abderrazak <abderrazak.htira@gmail.com>
2024-02-08 23:09:39 +0100

    refactor : change url

--- directory/urls.py
+++ directory/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'folder', view.FolderViewSet, basename='user')
 urlpatterns=[
-    path("Folder-content/<uuid:pk>/", view.GetFolder.as_view()),
+    path("folder-content/<uuid:pk>/", view.GetFolder.as_view()),
     path("root-folders/", view.GetRootFolders.as_view()),
     path("navigation-pane/", view.GetNavigationPane.as_view()),
 
