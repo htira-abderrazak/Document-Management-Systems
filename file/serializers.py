3d346d3de622b6a639c5e8724ec459e522266a74
htira-abderrazak <abderrazak.htira@gmail.com>
2024-03-26 14:51:44 +0100

    refactor : delete print

--- file/serializers.py
+++ file/serializers.py
@@ -41,7 +41,6 @@ def find_matching_folder(file_name, parent_folder_id):
 
     # Generate all possible substrings of length 3 from the file name
     substrings = {file_name[i:i+n] for n in range(3, len(file_name)+1) for i in range(len(file_name) - n + 1)}
-    print(substrings)
     # Search for folders with names containing any of the substrings
     matching_folders = Directory.objects.filter(parent=parent_folder, name__in=substrings)
     
