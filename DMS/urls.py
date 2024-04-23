98b9e8fadb2ca4cd5f31aa9e1b7d9c5960a1ca6f
htira-abderrazak <abderrazak.htira@gmail.com>
2024-04-23 23:23:58 +0100

    build : remove if debug = true

--- DMS/urls.py
+++ DMS/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     path('', include('file.urls')),
 
     path('admin/', admin.site.urls),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
