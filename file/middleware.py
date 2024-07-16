a124ae6c5a74eed1d371f3efb117b7f302fe580d
htira-abderrazak <abderrazak.htira@gmail.com>
2024-07-16 17:52:18 +0100

    fix : get user in the middle ware

--- file/middleware.py
+++ file/middleware.py
@@ -11,8 +11,9 @@ class MediaServeMiddleware:
         # Check if the request is for a media file
         if request.path.startswith('/uploads/'):
             try:
-                recent_files = Recent.objects.filter(user = request.user)
+                
                 file = File.objects.get(file = (request.path).lstrip("/"))
+                recent_files = Recent.objects.filter(user = file.user)
                 if not Recent.objects.filter(files = file,user = request.user).exists():
                     if recent_files.count() < 10 :
                         Recent(files =file).save()
