e45feddabdf1656541a5b0101b476a55c31215d2
htira-abderrazak <abderrazak.htira@gmail.com>
2024-07-22 17:33:18 +0100

    docs : comment the BASE_DIR variable of the sqllite database

--- DMS/settings.py
+++ DMS/settings.py
@@ -18,7 +18,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
