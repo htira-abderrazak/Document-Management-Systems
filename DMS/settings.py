e835faf2e2783d5414229f9540e70adbe8f43bab
htira-abderrazak <abderrazak.htira@gmail.com>
2025-02-22 16:04:28 +0100

    fix : add debug and allow host as env variables

--- DMS/settings.py
+++ DMS/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 STRIPE_SECRET_KEY=env("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 
