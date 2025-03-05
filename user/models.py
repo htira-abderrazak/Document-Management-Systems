8f66d2a5608b9f62754230e2e0b4ffa93ab1f2d6
htira-abderrazak <abderrazak.htira@gmail.com>
2025-03-05 16:28:47 +0100

    fix : change the default value to bytes

--- user/models.py
+++ user/models.py
@@ -10,4 +10,4 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     total_size = models.PositiveIntegerField(default=0)
-    max_size = models.PositiveIntegerField(default=100)
+    max_size = models.PositiveIntegerField(default=104857600)
