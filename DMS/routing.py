d8b3c39883e640c68e9b8b9a907fd8f3e7488b18
htira-abderrazak <abderrazak.htira@gmail.com>
2025-07-16 22:47:12 +0100

    chore : delete unnecessary url

--- DMS/routing.py
+++ DMS/routing.py
@@ -4,5 +4,5 @@ from django.urls import re_path
 from user.consumer import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<user_id>[^/]+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
 ]
