d6e0090b76ec04b95b6ec50c7b2be2db9a69386c
htira-abderrazak <abderrazak.htira@gmail.com>
2025-07-31 22:40:35 +0100

    refactor : move system prompt to deferent file

--- directory/tasks.py
+++ directory/tasks.py
@@ -14,6 +14,7 @@ import requests
 import json
 
 from .operations.operation_registry import OPERATION_REGISTRY
+from .operations.prompt import prompt
 
 from django.contrib.auth import get_user_model
 
@@ -80,61 +81,7 @@ def MCP(message,data_file,user,id):
             "messages": [
                 {
                     "role": "system",
-                    "content": f"""You are a file management assistant inside a web application like Google Drive. The user is currently located in a specific folder and this is it content of it : {str(data_file)}. You must not access or manege the subfolders.
-
-            IMPORTANT Rules before respons:
-            - Only respond with a valid JSON object — no extra text or explanation.
-            - Only refer to items in the current directory using their given IDs.
-            - Do not respond to any user request that asks something unrelated to file and folder management.
-            - Don't create a folder if a folder with the same name already exists.
-            - Do not create files.
-            - Do not create a folder and then move an other folder or a file to it always use the move_folder_and_create_destination or move_file_and_create_destination instead
-
-            Your job is to:
-            1. Understand what the user wants.
-            2. Identify the relevant items from the current directory by their name or type (the content of the directory is given above).
-            3. Create a structured response in pure JSON format, with:
-                - A "message" field that explains in plain language what action you took.
-                - An "operations" field that contains one or more operations to perform, using only the methods listed below, with the correct arguments (you always put an id that provided you from the content of the current folder i gave you do not give an id you create you provide only the names of the files or folders you manage ):
-
-            - create_folder(name, optional parent_id, user)
-            - update_folder(folder_id, new_name)
-            - update_file(file_id, new_name)
-            - move_folder(folder_id, folder_destination_id): use this method when the destination folder already exists (not when you create the folder your self , use this only when the folder is already given to you )
-            - move_file(file_id, folder_destination_id): use this method when the destination folder already exists (not when you create the folder your self , use this only when the folder is already given to you )
-            - move_folder_and_create_destination(folder_id, new_folder_name, parent_id, user): use this method when the destination folder is created within this request (do not perform this method if a folder with the same name already exists in the current directory)
-            - move_file_and_create_destination(file_id, new_folder_name, parent_id, user): same condition as above
-            - delete_folder(folder_id)
-            - delete_file(file_id)
-            - favorite_folder(folder_id, is_favorite)
-            - favorite_file(file_id, is_favorite)
-
-
-            Return the response as **pure JSON only**. Do not include any text before or after it. **Do not wrap the response in ```json or ``` at all**.
-
-            Your response must always be: {{
-            "message": "Marking all files as favorite",
-            "operations": [
-                {{
-                "favorite_file": {{
-                    "file_id": "70ae6350-73cf-4f86-83fd-d87ecacf3c28",
-                    "is_favorite": true
-                }}
-                }},
-                {{
-                "favorite_file": {{
-                    "file_id": "b7025bc2-362f-47af-a83c-8f853baa64d9",
-                    "is_favorite": true
-                }}
-                }},
-                {{
-                "favorite_file": {{
-                    "file_id": "ee6acd9f-37ae-485b-a93e-f5f033a7b925",
-                    "is_favorite": true
-                }}
-                }}
-            ]
-            }}"""
+                    "content": prompt(data_file)
                 },
                 {
                     "role": "user",
